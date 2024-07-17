3d05df41d42b42d4bf01b32f2ab6f5d366ecbead
stearm210 <pyzxwoo@163.com>
2024-07-17 16:04:07 +0800

    更改

--- tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -1,5 +1,7 @@
 package com.itheima.controller;
 
+import com.itheima.pojo.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,4 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DeptController {
+
+	//查询信息
+	@RequestMapping("/depts")
+	public Result list(){
+		System.out.println("查询全部部门数据");
+		return Result.success();
+	}
 }
