b119b20dfff6ef9fea78f01e3d325f66e78fc1fd
stearm210 <pyzxwoo@163.com>
2024-07-17 17:01:27 +0800

    更改

--- tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
+++ tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
@@ -13,7 +13,8 @@ import java.util.List;
 public interface DeptMapper {
 	/*
 	* 这个方法主要是用于查询全部的部门数据的
-	* mapper直接在接口中使用注解SQL语句的形式进行开发*/
+	* mapper直接在接口中使用注解SQL语句的形式进行开发
+	* */
 	@Select("select * from dept")
 
 	//在dept数据库当中查询到的信息会使用一个list进行返回
