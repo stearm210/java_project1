5db67a47e55c5163157549c9acd67dbdcd091fed
stearm210 <pyzxwoo@163.com>
2024-07-20 16:53:52 +0800

    更改

--- tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
@@ -6,15 +6,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Slf4j
 @RestController
 public class UploadController {
 
 	@PostMapping("/upload")
-	//服务端接收文件一般使用MultipartFile来接收，MultipartFile用于接收
-	//image对象
-	public Result upload(String username, Integer age, MultipartFile image){
+	//服务端接收文件一般使用MultipartFile来接收，MultipartFile用于接收文件、图片对象
+
+	public Result upload(String username, Integer age, MultipartFile image) throws IOException {
 		log.info("文件上传:{},{},{}",username,age,image);
+		//获取原始文件名
+		String originalFilename = image.getOriginalFilename();
+
+		//将文件存储到服务器的磁盘目录中
+		image.transferTo(new File("E:\\java study\\datebase\\"+originalFilename));
+
 		return Result.success();
 	}
 }
