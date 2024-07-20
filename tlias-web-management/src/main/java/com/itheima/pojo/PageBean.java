0d130e8d05ba5fa3ea6580e529a127d0feef8dad
stearm210 <pyzxwoo@163.com>
2024-07-20 09:10:52 +0800

    更改

---
+++ tlias-web-management/src/main/java/com/itheima/pojo/PageBean.java
@@ -0,0 +1,22 @@
+package com.itheima.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/*
+* 分页查询结果的封装类
+* */
+
+/*
+* 使用date、noargsconstructor、allargsconstructor注解来定义get、set、有参无参构造方法
+* */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageBean{
+	private Long total;//总记录数
+	private List rows;//数据列表
+}
