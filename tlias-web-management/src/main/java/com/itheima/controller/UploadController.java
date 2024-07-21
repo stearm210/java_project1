2a6c8fe5688422ee721a978eb35094f2b3e40298
stearm210 <pyzxwoo@163.com>
2024-07-21 10:02:50 +0800

    更改

--- tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -21,11 +22,19 @@ public class UploadController {
 		//获取原始文件名
 		String originalFilename = image.getOriginalFilename();
 
-		//构造唯一的上传文件名，防止被后续上传文件覆盖
+		//构造唯一的上传文件名，防止被后续上传文件覆盖 --uuid
+		//获取文件名最后一个点所在的位置
+		int index = originalFilename.lastIndexOf(".");
+		//获得文件的拓展名
+		String extname = originalFilename.substring(index);
+		//拼接uuid的后缀,使得变成一个新的文件名
+		String newFileName = UUID.randomUUID().toString()+extname;
+		//输出日志
+		log.info("新的文件名:{}",newFileName);
 
 
 		//将文件存储到服务器的磁盘目录中
-		image.transferTo(new File("E:\\java study\\datebase\\"+originalFilename));
+		image.transferTo(new File("E:\\java study\\datebase\\"+newFileName));
 
 		return Result.success();
 	}
